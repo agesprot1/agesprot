2e88b2be783553ddf1e563a87da6555d84e997bc
Adriann Sanchez <alka65@hotmail.com>
2016-05-14 12:49:14 -0500

    up

--- agesprot/urls.py
+++ agesprot/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.conf import settings
 
 urlpatterns = [
 	url(r'^admin/', admin.site.urls),
 	url(r'^', include('agesprot.apps.base.urls')),
 	url(r'^users/', include('agesprot.apps.users.urls')),
 	url(r'^project/', include('agesprot.apps.project.urls')),
+	url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
 ]
