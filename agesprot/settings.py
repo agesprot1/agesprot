679178644a2f8dc55efc4dfec363fa5f9ba79197
Adriann Sanchez <alka65@hotmail.com>
2016-08-14 17:54:59 -0500

    key app

--- agesprot/settings.py
+++ agesprot/settings.py
@@ -90,7 +90,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'agesprot1@gmail.com'
-EMAIL_HOST_PASSWORD = 'Agesprot.1'
+EMAIL_HOST_PASSWORD = 'ftvoyvddltwpylyl'
 EMAIL_PORT = 587
 # END
 
